a2a01c70d2ad34d2cc198b2ef789fb602b8d2eb9
HerryWEu <yihong.wu@mhp.com>
2024-08-18 01:31:32 +0800

    feat: application main

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan
+public class Main8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8001.class, args);
+    }
+}
