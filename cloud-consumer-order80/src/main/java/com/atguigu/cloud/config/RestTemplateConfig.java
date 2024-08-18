b203043f7c17fca2757931138a3ccaa76aa090b6
HerryWEu <yihong.wu@mhp.com>
2024-08-19 01:49:14 +0800

    feat: 切换负载均衡算法

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
@@ -1,16 +1,35 @@
 package com.atguigu.cloud.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(
+        //下面的value值大小写一定要和consul里面的名字一样，必须一样
+        value = "cloud-payment-service",configuration = RestTemplateConfig.class)
 public class RestTemplateConfig {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate()
     {
         return new RestTemplate();
+    }
+
+    @Bean
+    // 测试如何切换 负载均衡的算法，实际工作中基本轮询就够了
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
     }
 }
