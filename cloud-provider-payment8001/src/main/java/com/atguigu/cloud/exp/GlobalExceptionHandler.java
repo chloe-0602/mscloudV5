f7825ddf1f627f7a627f44a726714688cb0c5e34
HerryWEu <yihong.wu@mhp.com>
2024-08-18 11:17:30 +0800

    feat: 统一异常处理

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/exp/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.atguigu.cloud.exp;
+
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.resp.ReturnCodeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResultData<String> exceptionHandler(Exception e){
+        log.error("GlobalExceptionHandler: {}", e.getMessage(), e);
+        return ResultData.fail(ReturnCodeEnum.RC500.getCode(), e.getMessage());
+    }
+}
