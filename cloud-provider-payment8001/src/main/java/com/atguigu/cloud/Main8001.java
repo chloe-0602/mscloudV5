3c2ff8644e007920c08b9836b8d5c3f2c9957a14
HerryWEu <yihong.wu@mhp.com>
2024-08-19 00:22:48 +0800

    feat: consul分布式配置，动态refresh刷新

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -3,11 +3,13 @@ package com.atguigu.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.cloud.mapper")
 @EnableDiscoveryClient
+@RefreshScope //动态刷新
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
