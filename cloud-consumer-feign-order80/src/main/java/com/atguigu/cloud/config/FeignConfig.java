741ab4b13a0b6cb1495a677f0c20ecc6b9223a41
HerryWEu <yihong.wu@mhp.com>
2024-08-20 02:12:01 +0800

    feat: resilience4j CircuitBreaker 按照TIME_BASED 熔断的case

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -10,10 +10,10 @@ public class FeignConfig {
     @Bean
     public Retryer myRetryer()
     {
-//        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-        return new Retryer.Default(100,1,3);
+//        return new Retryer.Default(100,1,3);
     }
 
     @Bean
