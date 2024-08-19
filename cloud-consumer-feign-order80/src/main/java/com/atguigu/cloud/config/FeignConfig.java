ade9f192c07bebf98e22dff0a49b2dde60974d71
HerryWEu <yihong.wu@mhp.com>
2024-08-19 16:09:42 +0800

    feat: openfeign 开启日志，测试retry和请求响应压缩

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,14 @@ public class FeignConfig {
     @Bean
     public Retryer myRetryer()
     {
-        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+//        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-//        return new Retryer.Default(100,1,3);
+        return new Retryer.Default(100,1,3);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
