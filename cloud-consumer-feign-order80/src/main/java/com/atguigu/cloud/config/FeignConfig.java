5d622a31d9a586a73a5c6e6493316e5540c75a72
HerryWEu <yihong.wu@mhp.com>
2024-08-19 15:27:00 +0800

    feat: openfeign 定制重试次数

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -0,0 +1,17 @@
+package com.atguigu.cloud.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Retryer myRetryer()
+    {
+        //return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+
+        //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
+        return new Retryer.Default(100,1,3);
+    }
+}
