6c4e01d67b5f196d0c3453273035ecd567fd1b19
HerryWEu <yihong.wu@mhp.com>
2024-08-20 17:05:08 +0800

    feat: Gateway 可以通过gateway路由访问pay相关接口

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayGateWayController.java
@@ -0,0 +1,28 @@
+package com.atguigu.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.service.PayService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PayGateWayController
+{
+    @Resource
+    PayService payService;
+
+    @GetMapping(value = "/pay/gateway/get/{id}")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id){
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/gateway/info")
+    public ResultData<String> getGatewayInfo() {
+        return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+}
