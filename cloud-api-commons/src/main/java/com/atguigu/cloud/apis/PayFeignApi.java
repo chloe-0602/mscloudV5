88139cd0d72741bb4f50fc91084962efb5443acc
HerryWEu <yihong.wu@mhp.com>
2024-08-20 17:16:17 +0800

    feat: Gateway order通过gateway访问pay接口,而不是直接访问pay接口

--- cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "cloud-payment-service")
+//@FeignClient(value = "cloud-payment-service")
+@FeignClient(value = "cloud-gateway")
 public interface PayFeignApi {
     @PostMapping(value = "/pay/add")
     public ResultData addPay(@RequestBody PayDTO payDTO);
