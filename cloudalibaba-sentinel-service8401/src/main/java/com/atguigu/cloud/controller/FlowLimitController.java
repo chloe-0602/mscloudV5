ccf589b33d735b87e27624eb5ef477c0e84a775b
HerryWEu <yihong.wu@mhp.com>
2024-08-22 13:40:40 +0800

    feat: SpringCloud Alibaba: 整合sentinel入门案例

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -0,0 +1,23 @@
+package com.atguigu.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController
+{
+
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        return "------testB";
+    }
+}
+
+
