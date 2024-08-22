186cd7b0b4de662272ca13f4b0b6f181b4fb7af3
HerryWEu <yihong.wu@mhp.com>
2024-08-22 21:28:44 +0800

    feat: SpringCloud Alibaba: 测试sentinel流量控制模式中的链路

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.controller;
 
+import com.atguigu.cloud.service.FlowLimitService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,25 @@ public class FlowLimitController
     public String testB()
     {
         return "------testB";
+    }
+
+    /**流控-链路演示demo
+     * C和D两个请求都访问flowLimitService.common()方法，阈值到达后对C限流，对D不管
+     */
+    @Resource
+    private FlowLimitService flowLimitService;
+
+    @GetMapping("/testC")
+    public String testC()
+    {
+        flowLimitService.common();
+        return "------testC";
+    }
+    @GetMapping("/testD")
+    public String testD()
+    {
+        flowLimitService.common();
+        return "------testD";
     }
 }
 
