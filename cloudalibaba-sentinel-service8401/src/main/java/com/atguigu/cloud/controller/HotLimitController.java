8ee9de07a7f2fbdbdcf3a262441cf74c540e2dcd
HerryWEu <yihong.wu@mhp.com>
2024-08-23 15:25:18 +0800

    feat: SpringCloud Alibaba: 测试 热点参数限流

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/HotLimitController.java
@@ -0,0 +1,25 @@
+package com.atguigu.cloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HotLimitController {
+
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+    public String dealHandler_testHotKey(String p1, String p2, BlockException exception)
+    {
+        return "-----dealHandler_testHotKey";
+    }
+}
