aad10a524ffe1a62d650e37cdd22baa47a5e3a1a
HerryWEu <yihong.wu@mhp.com>
2024-08-23 12:00:07 +0800

    feat: SpringCloud Alibaba: 测试sentinel熔断降级 异常数量

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -75,6 +75,17 @@ public class FlowLimitController
         return "------testG,新增熔断规则-异常比例 ";
     }
 
+    /**
+     * 新增熔断规则-异常数
+     * @return
+     */
+    @GetMapping("/testH")
+    public String testH()
+    {
+        System.out.println("----测试:新增熔断规则-异常数 ");
+        int age = 10/0;
+        return "------testH,新增熔断规则-异常数 ";
+    }
 }
 
 
